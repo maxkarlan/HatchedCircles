6e4dd84c26cca10d4c13cc8ad5bf6aedff8f7456
maxkarlan <maxwell.karlan@gmail.com>
2023-11-19 23:31:04 -0600

    add several hatching styles including cross, contour, and scribble. re organized logic for colors for several hatch styles.

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
-// let canvasWidth = 450;
-// let canvasHeight = 700;
 let styleChoices = [];
 let circles = [];
 let maxSize = 80; // Maximum radius size
 let minSize = 2; // Minimum radius size
 let totalCircles = 5000; // Total number of circles to attempt to pack
 let attemptLimit = 5000; // Maximum attempts before moving to a smaller size
-// let skyBlueHSB = [197, 71, 88]; // HSB for sky blue
-// let deepRedHSB = [0, 100, 50];  // HSB for deep red
 sizeTier = 1;
 let denom = 3;
 let frameThickness = 15; // Thickness of the frame
 let frameToRectPad = 10; // Space between the frame and the rectangle range
-// let rectBound = 25;
-
 let rectX = [];
 let rectY = [];
 let rectWidth = [];
 let rectHeight = [];
 let rectBound = [];
 let bgChoices = [[0, 0, 0], [45, 20, 100], [200, 30, 100]];
+// let paletteChoices = [
+//    [[197, 100, 88], [0, 100, 50]],
+//    [[345, 100, 100], [40, 100, 100]],
+//    [[173, 100, 100], [244, 100, 100]],
+//    [[311, 100, 45], [193, 100, 29]],
+//    [[320, 100, 95], [60, 100, 90]]
+// ]
 let paletteChoices = [
-   [[197, 100, 88], [0, 100, 50]],
+   [[197, 100, 100], [0, 100, 100]],
    [[345, 100, 100], [40, 100, 100]],
    [[173, 100, 100], [244, 100, 100]],
-   [[311, 100, 45], [193, 100, 29]],
-   [[320, 100, 95], [60, 100, 90]]
+   [[311, 100, 100], [193, 100, 100]],
+   [[320, 100, 100], [60, 100, 100]]
 ]
+
 let lightPaletteChoices = [
    [[0, 0, 2], [187, 84, 67]]
 ]
@@ -46,16 +48,10 @@ function setup() {
    bgColor = random(bgChoices);
    // bgColor = bgChoices[2];
    console.log("bgColor:", bgColor);
-   styleNums = [1, 2];
+   styleNums = [1, 2, 3, 4];
    style = random(styleNums);
+   // style = 4;
    console.log("styleNum:", style);
-   isDark = random() < .5;
-   console.log("isDark:", isDark);
-   // if (isDark) {
-   //    palette = shuffle(random(darkPaletteChoices));
-   // } else {
-   //    palette = shuffle(random(lightPaletteChoices));
-   // }
    palette = shuffle(random(paletteChoices));
    color1 = palette[0];
    color2 = palette[1];
@@ -98,7 +94,6 @@ function setup() {
    }
    let currentSize = maxSize;
    let circleCounts = {}; // Track the number of circles for each size
-   // isDark = random() < .5
    while (circles.length < totalCircles && currentSize >= minSize) {
       let attempts = 0;
       let maxCircles;
@@ -112,12 +107,15 @@ function setup() {
             r: currentSize,
             color: getRandomColor(y / height),
             randomAngle: random(TWO_PI),
-            crossHatch: PI/2
+            crossHatch: PI/2,
+            scribbles: []
          };
+         if (style == 4) {
+            calculateScribbles(newCircle);
+         }
          if (!collides(newCircle)) {
             circles.push(newCircle);
             circleCounts[currentSize]++;
-            // console.log("Circle created with size: " + currentSize); // Log the size of each circle
          }
          attempts++;
       }
@@ -127,10 +125,6 @@ function setup() {
          denom = sizeTier + 1;
       }
    }
-
-   // rectX = random(((rectWidth/2) - (canvasWidth/2)), ((canvasWidth/2) - (rectWidth/2)));
-   // rectY = random(((rectHeight/2) - (canvasHeight/2)), ((canvasHeight/2) - (rectHeight/2)));
-
    // // Create and position the save button
    // saveButton = createButton('Save as JPG');
    // saveButton.position(10, height / 2); // Adjust the position as needed
@@ -155,51 +149,31 @@ function draw() {
    background(bgColor);
    translate(-width / 2, -height / 2); // Adjust for WEBGL's center origin
 
-   fill([0, 0, 0]);
-   // stroke(0);
-   // strokeWeight(frameThickness);
-   // rect(0, 0, frameThickness, height);
-   // rect(0, 0, width, frameThickness);
-   // rect(width - frameThickness, 0, frameThickness, height);
-   // rect(0, height - frameThickness, width, frameThickness);
-   // rect(0, 0, 50, 100);
-
-   // fill(100, 100, 100);
-   noStroke();
-   // rect(rectX, rectY, rectWidth, rectHeight);
-
    if (style == 1) {
       parallelHatch();
    } else if (style == 2) {
       crossHatch();
+   } else if (style == 3) {
+      contourHatch();
+   } else if (style ==4 ) {
+      scribbleHatch();
    }
-   // console.log("style:", random(styleChoices));
 }
 
 function parallelHatch() {
    for (let circle of circles) {
       let numLines = circle.r * 3; // Proportional to the radius
       let lineSpacing = (circle.r * 2) / numLines;
-      // let randomAngle = random(TWO_PI); // Random rotation angle
 
       push(); // Isolate transformations
       translate(circle.x, circle.y); // Move to the circle's center
       rotate(circle.randomAngle); // Apply rotation
-
-      if (isDark) {
-         // fillLight = 10;
-         // fill(color(hue(circle.color), fillLight, brightness(circle.color)));
-         // console.log("fillLight:", fillLight);
-         fill(0, 0, 20);
-      } else {
-         fill(0, 0, 80);
-         // fill(color(hue(circle.color), saturation(circle.color), fillDark));
-         // console.log("fillDark:", fillDark);
-      }
+      
+      fill(circle.color);
       noStroke();
       ellipse(0, 0, circle.r * 2, circle.r * 2);
 
-      stroke(circle.color); // Color of the hatching lines
+      stroke(0, 0, 0); // Color of the hatching lines
       strokeWeight(.2); // Thickness of the hatching lines
 
       for (let i = 0; i <= numLines; i++) {
@@ -212,9 +186,53 @@ function parallelHatch() {
    }
 }
 
+// function crossHatch() {
+//    for (let circle of circles) {
+//       let numLines = circle.r * 2; // Proportional to the radius
+//       let lineSpacing = (circle.r * 2) / numLines;
+//       // let randomAngle = random(TWO_PI); // Random rotation angle
+
+//       push(); // Isolate transformations
+//       translate(circle.x, circle.y); // Move to the circle's center
+//       rotate(circle.randomAngle); // Apply rotation
+
+//       if (isDark) {
+//          // fillLight = 10;
+//          // fill(color(hue(circle.color), fillLight, brightness(circle.color)));
+//          // console.log("fillLight:", fillLight);
+//          fill(0, 0, 20);
+//       } else {
+//          // fillDark = 10;
+//          // fill(color(hue(circle.color), saturation(circle.color), fillDark));
+//          // console.log("fillDark:", fillDark);
+//          fill(0, 0, 80);
+//       }
+//       noStroke();
+//       ellipse(0, 0, circle.r * 2, circle.r * 2);
+//       stroke(circle.color); // Color of the hatching lines
+//       strokeWeight(.25); // Thickness of the hatching lines
+
+//       for (let i = 0; i <= numLines; i++) {
+//          let y = -circle.r + i * lineSpacing;
+//          let xDelta = sqrt(circle.r * circle.r - y * y);
+//          line(-xDelta, y, xDelta, y);
+//       }
+
+//       rotate(circle.crossHatch); // Apply rotation
+
+//       for (let j = 0; j <= numLines; j++) {
+//          let y = -circle.r + j * lineSpacing;
+//          let xDelta = sqrt(circle.r * circle.r - y * y);
+//          line(-xDelta, y, xDelta, y);
+//       }
+      
+//       pop(); // Revert transformations
+//    }
+// }
+
 function crossHatch() {
    for (let circle of circles) {
-      let numLines = circle.r * 2; // Proportional to the radius
+      let numLines = circle.r * 1.5; // Proportional to the radius
       let lineSpacing = (circle.r * 2) / numLines;
       // let randomAngle = random(TWO_PI); // Random rotation angle
 
@@ -222,21 +240,12 @@ function crossHatch() {
       translate(circle.x, circle.y); // Move to the circle's center
       rotate(circle.randomAngle); // Apply rotation
 
-      if (isDark) {
-         // fillLight = 10;
-         // fill(color(hue(circle.color), fillLight, brightness(circle.color)));
-         // console.log("fillLight:", fillLight);
-         fill(0, 0, 20);
-      } else {
-         // fillDark = 10;
-         // fill(color(hue(circle.color), saturation(circle.color), fillDark));
-         // console.log("fillDark:", fillDark);
-         fill(0, 0, 80);
-      }
+      fill(circle.color);
       noStroke();
       ellipse(0, 0, circle.r * 2, circle.r * 2);
-      stroke(circle.color); // Color of the hatching lines
-      strokeWeight(.25); // Thickness of the hatching lines
+
+      stroke(0, 0, 0); // Color of the hatching lines
+      strokeWeight(.35); // Thickness of the hatching lines
 
       for (let i = 0; i <= numLines; i++) {
          let y = -circle.r + i * lineSpacing;
@@ -251,17 +260,54 @@ function crossHatch() {
          let xDelta = sqrt(circle.r * circle.r - y * y);
          line(-xDelta, y, xDelta, y);
       }
-      
+
       pop(); // Revert transformations
    }
 }
 
 function contourHatch(){
+   for (let circle of circles) {
+      let numContours = circle.r / 2; // Adjust the density of contour lines
+      let contourSpacing = circle.r / numContours;
 
+      push(); // Isolate transformations
+      translate(circle.x, circle.y); // Move to the circle's center
+      rotate(circle.randomAngle); // Apply rotation
+
+      fill(circle.color);
+      noStroke();
+      ellipse(0, 0, circle.r * 2, circle.r * 2);
+
+      stroke(0, 0, 0); // Color of the hatching lines
+      strokeWeight(.45); // Thickness of the hatching lines
+
+      // Draw each contour line
+      for (let i = 0; i < numContours; i++) {
+          let contourRadius = circle.r - i * contourSpacing;
+          if (contourRadius > 0) {
+              ellipse(0, 0, contourRadius * 2, contourRadius * 2);
+          }
+      }
+
+      pop(); // Revert transformations
+   }
 }
 
 function scribbleHatch() {
+   for (let circle of circles) {
+      push(); // Isolate transformations
+      translate(circle.x, circle.y); // Move to the circle's center
 
+      // Render pre-calculated scribbles
+      for (let scribble of circle.scribbles) {
+         noFill();
+         stroke(scribble.fillHue, saturation(circle.color), brightness(circle.color));
+         strokeWeight(1);
+         bezier(scribble.startX, scribble.startY, scribble.controlPoint1X, scribble.controlPoint1Y, scribble.controlPoint2X, scribble.controlPoint2Y, scribble.endX, scribble.endY);
+      }
+
+      pop(); // Revert transformations
+   }
 }
 
 function stippleHatch() {
@@ -276,22 +322,13 @@ function getRandomColor(position) {
    // Define the mean and standard deviation for the normal distribution
    let hueMean = lerp(color1[0], color2[0], position);
    let hueSD = 40; // Standard deviation
-   let sat = lerp(color1[1], color2[1], position);
-   // let satSD = 10;
-   let bright = lerp(color1[2], color2[2], position);
-   // let brightSD = 10;
  
    // Generate a random hue value based on the normal distribution
    let hue = randomGaussian(hueMean, hueSD);
-   // let sat = randomGaussian(satMean, satSD);
-   // let bright = randomGaussian(brightMean, brightSD);
  
    // Keep the hue within the 0-360 range
    hue = (hue + 360) % 360;
-   // sat = (sat + 100) % 100;
-   // bright = (bright + 100) % 100;
- 
-   // Return the HSB color
+
    return color(hue, 100, 100);
 }
 
@@ -302,11 +339,6 @@ function collides(circle) {
          return true;
       }
    }
-   //   // Check collision with the frame
-   // let frameInnerEdgeX = framePadding + frameThickness / 2;
-   // let frameInnerEdgeY = framePadding + frameThickness / 2;
-   // let frameOuterEdgeX = width - framePadding - frameThickness / 2;
-   // let frameOuterEdgeY = height - framePadding - frameThickness / 2;
 
    // Check collision with the frame
    let frameInnerEdgeX = frameThickness;
@@ -320,26 +352,6 @@ function collides(circle) {
       circle.y + circle.r > frameOuterEdgeY) {
       return true;
    }
-
-//    // Check collision with the rectangle
-//    if (circle.x + circle.r > rectX - rectWidth / 2 &&
-//       circle.x - circle.r < rectX + rectWidth / 2 &&
-//       circle.y + circle.r > rectY - rectHeight / 2 &&
-//       circle.y - circle.r < rectY + rectHeight / 2) {
-//       return true;
-//   }
-
-   // // Adjust the rectangle coordinates for WEBGL
-   // let adjustedRectX = rectX + width / 2;
-   // let adjustedRectY = rectY + height / 2;
-
-   // // Check collision with the rectangle
-   // if (circle.x + circle.r > rectX - rectWidth / 2 &&
-   //    circle.x - circle.r < rectX + rectWidth / 2 &&
-   //    circle.y + circle.r > rectY - rectHeight / 2 &&
-   //    circle.y - circle.r < rectY + rectHeight / 2) {
-   //    return true;
-   // }
 
    // Check collision with the rectangle
    for (let i = 0; i < numRect; i++) {
@@ -368,4 +380,29 @@ function collides(circle) {
    }
 
    return false;
+}
+
+function calculateScribbles(circle) {
+   let numScribbles = (PI * circle.r * circle.r) / 5; // Proportional to the area
+
+   for (let i = 0; i < numScribbles; i++) {
+       let startX = random(-circle.r, circle.r);
+       let startY = random(-circle.r, circle.r);
+       let endX = random(-circle.r, circle.r);
+       let endY = random(-circle.r, circle.r);
+
+       // Ensure start and end points are within the circle
+       if (dist(0, 0, startX, startY) < circle.r && dist(0, 0, endX, endY) < circle.r) {
+           let controlPoint1X = random(-circle.r, circle.r);
+           let controlPoint1Y = random(-circle.r, circle.r);
+           let controlPoint2X = random(-circle.r, circle.r);
+           let controlPoint2Y = random(-circle.r, circle.r);
+           let hueDiff = random(-20, 20);
+           let fillHue = (hue(circle.color) + hueDiff);
+           fillHue = (fillHue + 360) % 360;
+
+           // Store each scribble
+           circle.scribbles.push({startX, startY, controlPoint1X, controlPoint1Y, controlPoint2X, controlPoint2Y, endX, endY, fillHue});
+       }
+   }
 }
